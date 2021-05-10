bd00062d11b57404f1ccf2077e1c01780e8acca3
Lbniese <lbniese@gmail.com>
2021-05-11 01:10:57 +0200

    Added mysql and dotenv usage
    *Added mysql and dotenv usage

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
+const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: path.resolve(__dirname, '.env')});
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
+
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASS,
+  database: process.env.MYSQL_DB,
+});
 
 app.use(express.static('public'));
 app.use(express.json());
@@ -20,3 +34,12 @@ const server = app.listen(port, (error) => {
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/public/index.html`, 'utf-8');
 });
+
+app.get('/mysql', (req, res) => {
+  connection.connect(function(err) {
+    if (err) throw err;
+    const sql = connection.format('SHOW STATUS;');
+    console.log(sql);
+    console.log(connection.query(sql));
+  });
+})
