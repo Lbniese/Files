38e4672fc49db76841af669d0e502cb4f40ff8cc
Lbniese <Lbniese@users.noreply.github.com>
2021-05-19 15:07:50 +0200

    optimized login.sql script

--- login.sql
+++ login.sql
@@ -1,12 +1,14 @@
 USE `heroku_41151142c290339`;
 
+DROP TABLE IF EXISTS `accounts`;
+
 CREATE TABLE IF NOT EXISTS `accounts` (
-  `id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL UNIQUE,
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 INSERT INTO `accounts` (`id`, `username`, `password`) VALUES (1, 'user', 'user');
+INSERT INTO `accounts` (`id`, `username`, `password`) VALUES (2, 'root', 'ascent');
 
-ALTER TABLE `accounts` ADD PRIMARY KEY (`id`);
-ALTER TABLE `accounts` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+SELECT * FROM accounts;
