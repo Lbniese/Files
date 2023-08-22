775fb3a9524c62daa932af8ec7da0eb237c56744
Lukas Bjerrum-Niese <Lbniese@gmail.com>
2023-08-22 10:53:42 +0200

    Core: Cleaner implementation

--- app.js
+++ app.js
@@ -1,65 +1,78 @@
-// import 'path' module which provides utilities for woking with file and directory paths
+// Modules for core functionality
 const path = require('path');
-// import 'dotenv' module that loads environment variables form a .env file into process.env
 const dotenv = require('dotenv');
+const express = require('express');
+const session = require('express-session');
+const fs = require('fs');
+const http = require('http');
+const io = require('socket.io');
 
-// load .env file contents into process.env from the root paths
+// Load environment variables
 dotenv.config({ path: path.resolve(__dirname, '.env') });
 
-// import 'express' module which is a web framework for node
-const express = require('express');
-
-// initiate expresss
+// Initialize Express
 const app = express();
 
-// import 'express-session' module which is a session middleware
-const session = require('express-session');
-
-// defining port to be environment variable PORT or else 8080
+// Configuration
 const port = process.env.PORT || 8080;
 
-// using session middleware
+// Static content loaded once during app startup
+const header = fs.readFileSync(`${__dirname}/public/header.html`, 'utf-8');
+const footer = fs.readFileSync(`${__dirname}/public/footer.html`, 'utf-8');
+const loginpage = fs.readFileSync(`${__dirname}/public/login.html`, 'utf-8');
+const filespage = fs.readFileSync(`${__dirname}/public/files.html`, 'utf-8');
+const registerpage = fs.readFileSync(`${__dirname}/public/register.html`, 'utf-8');
+
+// Middlewares
 app.use(session({
-  // secret is used to sign the session ID cookie - DEPRECATED
   secret: process.env.SECRET,
-  // maxAge - session lasts for 15min
   maxAge: 900000,
-  // resave forces the session to be saved back to the session store
   resave: true,
-  // saveUnitialized forces the session that is unitialized to be saved in the session store as well
   saveUninitialized: true,
 }));
-
-// defining location where static files should be served from
 app.use(express.static('public'));
-// using middleware that parses json and looks only at requests
 app.use(express.json());
-// using middleware that parses urlencoded bodies and looks at requests
 app.use(express.urlencoded({ extended: true }));
 
-// The 'fs' module enables interacting with the file system
-const fs = require('fs');
-
-// returns http server object with the requestListener being express
-// the requestListener is being executed every time the server gets a request
-const server = require('http').createServer(app);
-
-// import socket.io and connect it to the http server 'server'
-const io = require('socket.io')(server);
-
-// import auth handler router
+// Routers
 const authHandlerRouter = require('./routes/authHandler.js');
-// import file handler router
 const fileHandlerRouter = require('./routes/fileHandler.js');
-// import ratelimit handler router
 const rateLimitHandlerRouter = require('./routes/rateLimitHandler.js');
 
-// use the imported routes
 app.use(authHandlerRouter.router);
 app.use(fileHandlerRouter.router);
 app.use(rateLimitHandlerRouter.defaultLimiter);
 
-// listen to a port and start web server
+// Routes
+app.get('/', (req, res) => res.send(header + loginpage + footer));
+app.get('/register', (req, res) => res.send(header + registerpage + footer));
+app.get('/files', (req, res) => {
+    if (req.session.loggedin) {
+        res.send(header + filespage + footer);
+    } else {
+        res.send('Please make sure that you are logged in');
+    }
+});
+
+// Socket setup
+const server = http.createServer(app);
+const socket = io(server);
+
+let user = null;  // Initialize empty user
+
+socket.on('connection', (client) => {
+  client.username = user;
+
+  client.on('new_message', (data) => {
+    socket.emit('new_message', { message: data.message, username: client.username });
+  });
+
+  client.on('typing', () => {
+    client.broadcast.emit('typing', { username: client.username });
+  });
+});
+
+// Start server
 server.listen(port, (error) => {
   if (error) {
     console.log(error);
@@ -67,52 +80,3 @@ server.listen(port, (error) => {
     console.log(`Server listening on ${port}!`);
   }
 });
-
-// define locations of the different .html pages
-const header = fs.readFileSync(`${__dirname}/public/header.html`, 'utf-8');
-const footer = fs.readFileSync(`${__dirname}/public/footer.html`, 'utf-8');
-const loginpage = fs.readFileSync(`${__dirname}/public/login.html`, 'utf-8');
-const filespage = fs.readFileSync(`${__dirname}/public/files.html`, 'utf-8');
-const registerpage = fs.readFileSync(`${__dirname}/public/register.html`, 'utf-8');
-
-// initialize empty user
-let user = null;
-
-// get call that serves login.html
-app.get('/', (req, res) => {
-  res.send(header + loginpage + footer);
-});
-
-// get call that serves the register page
-app.get('/register', (req, res) => {
-  res.send(header + registerpage + footer);
-});
-
-// get call that serves files.html if the session states that the user is logged in
-app.get('/files', (req, res) => {
-  if (req.session.loggedin) {
-    user = req.session.username;
-    res.send(header + filespage + footer);
-  } else {
-    res.send('Please make sure that you are logged in');
-  }
-  // res.end();
-});
-
-// SOCKET - START
-io.on('connection', (socket) => {
-  // set username as the one we logged in with (auth, from mysql db)
-  socket.username = user;
-
-  // listen on new_message
-  socket.on('new_message', (data) => {
-    // broadcast the new message
-    io.emit('new_message', { message: data.message, username: socket.username });
-  });
-
-  // listen on typing - be able to show username of the one typing
-  socket.on('typing', () => {
-    socket.broadcast.emit('typing', { username: socket.username });
-  });
-});
-// SOCKET - END
