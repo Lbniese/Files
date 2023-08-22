93cd1be10ff643a7f807ffdc7d62737a813a1cab
Lukas Bjerrum-Niese <Lbniese@gmail.com>
2023-08-22 10:54:05 +0200

    Core: Reworked auth handler

--- routes/authHandler.js
+++ routes/authHandler.js
@@ -1,82 +1,62 @@
-// import express's Router() - isolated instance of middleware and routes (mini-application)
-// - can only perform middwalre and routing functions
 const router = require('express').Router();
-
-// import 'mysql' module which is a driver for node js
 const mysql = require('mysql');
-
 const bcrypt = require('bcrypt');
 
-// salt sounds - the more salt the longer time it takes to bruteforce
-const salt = 11;
+const SALT_ROUNDS = 11;
 
-// defining mysql connection details for when we want to connect to the database
-// const con = mysql.createConnection({
-const con = mysql.createPool({
+// Using a connection pool can help with better performance, handling multiple connections.
+const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASS,
   database: process.env.MYSQL_DB,
 });
 
-// post call handling logic for users looking to register and serving the register.html page
-router.post('/register', (req, res) => {
-  const { username } = req.body;
-  const { password } = req.body;
+router.post('/register', async (req, res) => {
+  const { username, password } = req.body;
 
-  // checking if both username and password fields have input data
-  if (username && password) {
-  // insert data into the database with query
-    bcrypt.hash(password, salt, (err, hash) => {
-      con.query('INSERT INTO accounts (username, password) VALUES (?, ?)', [username, hash], (error, results, fields) => {
-        if (error) {
-          console.log(error);
-          // we assume the only error that can occur is that username is already in use
-          res.send('Username is already taken');
-        } else {
-        // redirecting to homepage (login page) no matter what if no errors
-          if (results.length > 0) {
-            res.redirect('/');
-          } else {
-            res.redirect('/');
-          }
-          res.end();
-        }
-      });
+  if (!username || !password) {
+    return res.end();
+  }
+
+  try {
+    const hash = await bcrypt.hash(password, SALT_ROUNDS);
+    pool.query('INSERT INTO accounts (username, password) VALUES (?, ?)', [username, hash], (error, results) => {
+      if (error) {
+        console.log(error);
+        res.send('Username is already taken');
+      } else {
+        res.redirect('/');
+      }
     });
-  } else {
-    console.log(res.end());
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Server error');
   }
 });
 
-// post call to handle authentication
 router.post('/auth', (req, res) => {
-  const { username } = req.body;
-  const { password } = req.body;
-  // checking if both username and password fields have input data
-  if (username && password) {
-    // check if username and pass is in db
-    con.query('SELECT * FROM accounts WHERE username = ?', [username], (error, rows, fields) => {
-      if (error) {
-        console.log(error);
-      } else if (rows.length > 0) {
-        if (bcrypt.compare(password, rows[0].password)) {
-          // if success then set 'loggedin' to true
-          req.session.loggedin = true;
-          // setting username data as well which is used in the sockets chat
-          req.session.username = username;
-          res.redirect('/files');
-        } else {
-          // if no result returned we assume there was an error
-          console.log(res.send('Incorrect Username and/or Password!'));
-        }
-        res.end();
-      } else {
-        console.log(res.send('Please enter correct Username and/or Password!'));
-        console.log(res.end());
-      }
-    });
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.end();
   }
+
+  pool.query('SELECT * FROM accounts WHERE username = ?', [username], async (error, rows) => {
+    if (error) {
+      console.log(error);
+      return res.end();
+    }
+
+    if (rows.length > 0 && await bcrypt.compare(password, rows[0].password)) {
+      req.session.loggedin = true;
+      req.session.username = username;
+      return res.redirect('/files');
+    }
+
+    console.log('Incorrect Username and/or Password!');
+    res.send('Please enter correct Username and/or Password!').end();
+  });
 });
 
 module.exports = {
