67cf5b9c90bbfd59a66e7cf7fc00642bc464978f
Lukas Bjerrum-Niese <Lbniese@gmail.com>
2023-08-22 11:54:38 +0200

    DB: Ported from MySQL to PostgreSQL

--- login.sql
+++ login.sql
@@ -1,14 +1,12 @@
-USE `bvtmm146etmsbzx4`;
+DROP TABLE IF EXISTS accounts CASCADE;
 
-DROP TABLE IF EXISTS `accounts`;
+CREATE TABLE IF NOT EXISTS accounts (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(50) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL
+);
 
-CREATE TABLE IF NOT EXISTS `accounts` (
-  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL UNIQUE,
-  `password` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
-#INSERT INTO `accounts` (`id`, `username`, `password`) VALUES (1, 'user', '$2b$11$BJDYOCs7cq.PsXg.XACdW.v/hZ910o98jm3ywgsXvmEBuhxLwrMjG');
-#INSERT INTO `accounts` (`id`, `username`, `password`) VALUES (2, 'root', '$2b$11$5dEjvZTU1AhqiqHvTIq1fOPlRJLDr1RlZxU9mUQbd79ekysRHH3Oy');
+INSERT INTO accounts (id, username, password) VALUES (1, 'user', '$2b$11$BJDYOCs7cq.PsXg.XACdW.v/hZ910o98jm3ywgsXvmEBuhxLwrMjG');
+INSERT INTO accounts (id, username, password) VALUES (2, 'root', '$2b$11$5dEjvZTU1AhqiqHvTIq1fOPlRJLDr1RlZxU9mUQbd79ekysRHH3Oy');
 
 SELECT * FROM accounts;
--- routes/authHandler.js
+++ routes/authHandler.js
@@ -1,15 +1,18 @@
 const router = require('express').Router();
-const mysql = require('mysql');
+const { Pool } = require('pg');
 const bcrypt = require('bcrypt');
 
 const SALT_ROUNDS = 11;
 
 // Using a connection pool can help with better performance, handling multiple connections.
-const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASS,
-  database: process.env.MYSQL_DB,
+
+const pool = new Pool({
+  host: process.env.POSTGRES_HOST,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASS,
+  database: process.env.POSTGRES_DB,
+  port: process.env.POSTGRES_PORT || 5432, // Default PostgreSQL port
+  ssl: { rejectUnauthorized: false }
 });
 
 router.post('/register', async (req, res) => {
@@ -21,7 +24,8 @@ router.post('/register', async (req, res) => {
 
   try {
     const hash = await bcrypt.hash(password, SALT_ROUNDS);
-    pool.query('INSERT INTO accounts (username, password) VALUES (?, ?)', [username, hash], (error, results) => {
+    
+    pool.query('INSERT INTO accounts (username, password) VALUES ($1, $2)', [username, hash], (error, results) => {
       if (error) {
         console.log(error);
         res.send('Username is already taken');
@@ -29,6 +33,7 @@ router.post('/register', async (req, res) => {
         res.redirect('/');
       }
     });
+
   } catch (err) {
     console.log(err);
     res.status(500).send('Server error');
@@ -42,20 +47,32 @@ router.post('/auth', (req, res) => {
     return res.end();
   }
 
-  pool.query('SELECT * FROM accounts WHERE username = ?', [username], async (error, rows) => {
+  pool.query('SELECT * FROM accounts WHERE username = $1', [username], async (error, results) => {
     if (error) {
       console.log(error);
       return res.end();
     }
 
-    if (rows.length > 0 && await bcrypt.compare(password, rows[0].password)) {
-      req.session.loggedin = true;
-      req.session.username = username;
-      return res.redirect('/files');
+    try {
+      if (results.rows.length > 0) {
+        const user = results.rows[0];
+        
+        if (user && await bcrypt.compare(password, user.password)) {
+          req.session.loggedin = true;
+          req.session.username = username;
+          return res.redirect('/files');
+        } else {
+          console.log('Incorrect Username and/or Password!');
+          res.send('Please enter correct Username and/or Password!').end();
+        }
+      } else {
+        console.log('Incorrect Username and/or Password!');
+        res.send('Please enter correct Username and/or Password!').end();
+      }
+    } catch (err) {
+      console.error("Error comparing passwords:", err);
+      return res.status(500).send('Server error');
     }
-
-    console.log('Incorrect Username and/or Password!');
-    res.send('Please enter correct Username and/or Password!').end();
   });
 });
 
